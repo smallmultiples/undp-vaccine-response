c3cdd046bb33d21522027dde75b891cfe029e909
Andrea Lau <andrea@small.mu>
2020-06-04 16:59:09 +1000

    little chart fixes

--- react-frontend/src/components/questions/questions.jsx
+++ react-frontend/src/components/questions/questions.jsx
@@ -82,10 +82,10 @@ const Question = props => {
                 });
             }
             const isNumericData = tmp.every(t => typeof t.data === "number" || t.data === "");
-            if (tmp.length > 0 && isNumericData) {
+            if (tmp.length > 0 && isNumericData && tmp != "") {
                 return {
                     indicator: ind,
-                    data: tmp,
+                    data: tmp.filter(d => d.data != ""),
                 };
             } else {
                 return undefined;
