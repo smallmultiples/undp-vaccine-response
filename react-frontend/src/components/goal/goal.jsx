3ad66006417a1abbdbc2d7aa62f1aca36ca7961f
Harry Morris <harry@small.mu>
2020-07-29 16:23:42 +1000

    WIP

--- react-frontend/src/components/goal/goal.jsx
+++ react-frontend/src/components/goal/goal.jsx
@@ -1,5 +1,54 @@
 import React from "react";
+import Map from "../map/map";
 
 export default function Goal(props) {
-    const { slug } = props;
+    const { goal, pillar, pillarData } = props;
+    const { pillars, data, regionLookup, loading } = pillarData;
+
+    const covidPillar = React.useMemo(() => {
+        if (!pillars) return null;
+        return pillars.find(d => d.covid);
+    }, [pillars]);
+
+    // TODO: replace
+    // Countrydata is just a merge of all the datasets
+    const countryData = React.useMemo(() => {
+        if (loading) return null;
+
+        let newData = {};
+        Object.values(regionLookup).forEach(region => {
+            newData[region["ISO-alpha3 Code"]] = region;
+        });
+        data.forEach(row => {
+            const rowKey = row["Alpha-3 code"];
+            newData[rowKey] = newData[rowKey] || {};
+
+            Object.entries(row).forEach(([key, value]) => {
+                newData[rowKey][key] = value;
+            });
+        });
+        return newData;
+    }, [data, loading, regionLookup]);
+
+    return (
+        <React.Fragment>
+            <Map
+                countryData={countryData}
+                countryDataLoading={loading}
+                activePillar={pillar}
+                covidPillar={covidPillar}
+                pillars={pillars}
+                activeQuestion={goal}
+            />
+            {/*!isMapOnly && (
+            <Questions
+                activePillar={activePillar}
+                covidPillar={covidPillar}
+                datasets={datasets}
+                countryData={countryData}
+                hdiIndicator={hdiIndicator}
+            />
+        )*/}
+        </React.Fragment>
+    );
 }
--- react-frontend/src/pages/pillar/pillar.jsx
+++ react-frontend/src/pages/pillar/pillar.jsx
@@ -1,9 +1,5 @@
 import axios from "axios";
-import { flatten, uniq } from "lodash";
 import React from "react";
-import Map from "../../components/map/map";
-import Pillars from "../../components/pillars/pillars";
-import Questions from "../../components/questions/questions";
 import {
     DATA_SHEET_URL,
     PILLAR_URL,
@@ -14,145 +10,64 @@ import {
 import parseMetaSheet from "../../modules/data/parse-meta-sheet";
 import { isMapOnly } from "../../modules/is-map-only";
 import styles from "./pillar.module.scss";
+import Goal from "../../components/goal/goal";
 
 const usePillarData = () => {
     const [pillars, setPillars] = React.useState(null);
     const [regionLookup, setRegionLookup] = React.useState(null);
-    const [datasets, setDatasets] = React.useState({});
     const [loading, setLoading] = React.useState(true);
+    const [pillarData, setPillarData] = React.useState(null);
 
     React.useEffect(() => {
-        (async () => {
-            const pillars = await axios(PILLAR_URL).then(d => parseMetaSheet(d.data));
-            setPillars(pillars);
+        Promise.all([
+            axios(PILLAR_URL)
+                .then(res => parseMetaSheet(res.data))
+                .then(setPillars),
 
-            const regionsPromise = axios(REGIONS_URL)
-                .then(d => d.data)
-                .then(setRegionLookup);
-
-            let newSets = {};
-
-            if (USE_SHEET) {
-                const sheetsToFetch = uniq(pillars.map(p => p.sheet).filter(Boolean));
-
-                await Promise.all(
-                    sheetsToFetch.map(async sheet => {
-                        const res = await axios(`${DATA_SHEET_URL}?range=${sheet}`);
-                        newSets[sheet] = res.data;
-                    })
-                );
-            } else {
-                newSets = await axios(`${STATIC_DATA_BASE_URL}/datasets.json`).then(d => d.data);
-            }
-            await regionsPromise;
-
-            setDatasets(newSets);
-            setLoading(false);
-        })();
+            axios(REGIONS_URL)
+                .then(res => res.data)
+                .then(setRegionLookup),
+        ]);
     }, []);
 
-    // Countrydata is just a merge of all the datasets
-    const countryData = React.useMemo(() => {
-        if (loading) return null;
+    const pillar = React.useMemo(() => {
+        if (!pillars) return null;
+        return pillars[1]; // TODO
+    });
 
-        let data = {};
-        Object.values(regionLookup).forEach(region => {
-            data[region["ISO-alpha3 Code"]] = region;
-        });
-        Object.values(datasets).forEach(dataset => {
-            dataset.forEach(row => {
-                const rowKey = row["Alpha-3 code"];
-                data[rowKey] = data[rowKey] || {};
+    React.useEffect(() => {
+        if (!pillar) return;
+        // TODO: add static per pillar files. Export required too.
+        axios(`${DATA_SHEET_URL}?range=${pillar.sheet}`)
+            .then(d => d.data)
+            .then(setPillarData)
+            .then(() => setLoading(false));
+    }, [pillar]);
 
-                Object.entries(row).forEach(([key, value]) => {
-                    data[rowKey][key] = value;
-                });
-            });
-        });
-        return data;
-    }, [datasets, loading, regionLookup]);
+    // TODO: remove "pillars".
+    // TODO: remove special "covid" sheet.
 
     return {
-        countryData,
+        pillar,
         loading,
-        datasets,
         pillars,
+        regionLookup,
+        data: pillarData,
     };
 };
 
 export default function Pillar(props) {
-    const { pillars, datasets, countryData, loading } = usePillarData();
-    const [activeQuestion, setActiveQuestion] = React.useState(null);
+    const pillarData = usePillarData();
+    // TODO: pillar must be global state.
+    const { pillar, data, regionLookup, loading } = pillarData;
 
-    React.useEffect(() => {
-        if (activeQuestion || !pillars) return;
-        const goals = flatten(pillars.filter(d => d.visible).map(p => p.goals));
-        setActiveQuestion(goals[0]);
-    }, [pillars, activeQuestion]);
+    if (!pillar) return null; // TODO loader
 
-    const covidPillar = React.useMemo(() => {
-        if (!pillars) return null;
-        return pillars.find(d => d.covid);
-    }, [pillars]);
-
-    const lastUpdatedDate = React.useMemo(() => {
-        if (!covidPillar) return undefined;
-        return new Date(
-            covidPillar.goals[0].indicators[0].meta.updateFrequency.replace("Last updated ", "") +
-                " GMT+1000"
-        );
-    }, [covidPillar]);
-
-    const activePillar = React.useMemo(() => {
-        if (!pillars) return null;
-        return pillars.find(pillar => pillar.goals.some(q => q === activeQuestion));
-    }, [activeQuestion, pillars]);
-
-    const hdiIndicator = React.useMemo(() => {
-        if (!pillars) return null;
-        const indicators = flatten(pillars.map(p => flatten(p.goals.map(q => q.indicators))));
-        return indicators.find(d => d.hdi);
-    }, [pillars]);
-
-    if (!pillars || !activePillar || !activeQuestion) return null; // TODO loader
-
-    const dateTimeFormat = new Intl.DateTimeFormat("en", {
-        year: "numeric",
-        month: "short",
-        day: "2-digit",
-    });
-    const date = dateTimeFormat.format(lastUpdatedDate);
+    console.log({ pillar });
 
     return (
         <React.Fragment>
-            <Map
-                countryData={countryData}
-                countryDataLoading={loading}
-                activePillar={activePillar}
-                covidPillar={covidPillar}
-                pillars={pillars}
-                activeQuestion={activeQuestion}
-            />
-            {!isMapOnly && (
-                <Questions
-                    activePillar={activePillar}
-                    covidPillar={covidPillar}
-                    datasets={datasets}
-                    countryData={countryData}
-                    hdiIndicator={hdiIndicator}
-                />
-            )}
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <div className={styles.subHeadings}>
-                <span className={styles.updateDate}>
-                    Data last updated <em>{date}</em>
-                </span>
-            </div>
+            <Goal goal={pillar.goals[0]} pillar={pillar} pillarData={pillarData} />
         </React.Fragment>
     );
 }
