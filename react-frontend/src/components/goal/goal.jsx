83e0635cae9a623ea83794d4908c936d6523e059
Harry Morris <harry.t.morris@gmail.com>
2020-08-04 10:51:28 +1000

    indicator state hook in file

--- react-frontend/src/components/goal/goal.jsx
+++ react-frontend/src/components/goal/goal.jsx
@@ -1,75 +1,14 @@
-import { flatten } from "lodash";
 import React from "react";
 import Map from "../map/map";
 import styles from "./goal.module.scss";
 import TimeSliderTemp from "./time-slider-temp.svg";
+import { useIndicatorState } from "./useIndicatorState";
 
 export const TIMELINE_SCALES = {
     Yearly: 1,
     Monthly: 2,
     Daily: 3,
 };
-
-const getDefaultIndicatorState = (pillar, goal) => {
-    // TODO: module
-    const bivariateYOptions = flatten(pillar.goals.map(d => d.indicators));
-
-    const mapVisualisationOptions = goal.indicators.filter(d => d.isProgressIndicator);
-
-    return {
-        // Question indicator is the X axis
-        bivariateX: goal.indicators[0],
-        bivariateXEnabled: true,
-        // Any indicator for the pillar on the Y axis
-        bivariateY: bivariateYOptions.length > 1 ? bivariateYOptions[1] : bivariateYOptions[0],
-        bivariateYEnabled: false,
-        // "above-map" layer, be it simple circles or whatever. Is a "progress indicator".
-        mapVisualisation: mapVisualisationOptions[0],
-        mapVisualisationEnabled: mapVisualisationOptions.length > 0,
-    };
-};
-
-function useIndicatorState(pillar, goal) {
-    const [currentIndicators, setCurrentIndicators] = React.useState(
-        getDefaultIndicatorState(pillar, goal)
-    );
-
-    // TODO: remove most of this logic. goal and pillar won't change.
-
-    React.useEffect(() => {
-        if (!pillar) return;
-        // Whenever active pillar changes, set the pillar indicator (Y) to the first avail.
-        const bivariateYOptions = flatten(pillar.goals.map(d => d.indicators)).filter(
-            d => !d.categorical
-        );
-        setCurrentIndicators(d => ({
-            ...d,
-            bivariateY: bivariateYOptions.length > 1 ? bivariateYOptions[1] : bivariateYOptions[0],
-        }));
-    }, [pillar]);
-
-    React.useEffect(() => {
-        if (!goal) return;
-        if (goal.categorical) {
-            setCurrentIndicators(d => ({
-                ...d,
-                bivariateX: goal.indicators.filter(d => !d.categorical)[0],
-                bivariateXEnabled: false,
-                bivariateYEnabled: false,
-                mapVisualisationEnabled: true,
-            }));
-        } else {
-            // Whenever active QUESTION changes, set the pillar indicator to the first for the question
-            setCurrentIndicators(d => ({
-                ...d,
-                bivariateX: goal.indicators[0],
-                bivariateXEnabled: true,
-                bivariateYEnabled: false,
-            }));
-        }
-    }, [goal]);
-    return [currentIndicators, setCurrentIndicators];
-}
 
 export default function Goal(props) {
     const { goal, pillar, regionLookup, pillarLoading, goalDatasets } = props;
