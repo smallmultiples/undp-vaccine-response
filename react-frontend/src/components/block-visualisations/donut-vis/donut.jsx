be303b310d3d743770300171e940d1f29fed22a0
Harry Morris <harry@small.mu>
2020-08-11 12:52:26 +1000

    use format

--- react-frontend/src/components/block-visualisations/donut-vis/donut.jsx
+++ react-frontend/src/components/block-visualisations/donut-vis/donut.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { getBlockVisValue } from "../block-vis-utils";
+import styles from "./donut.module.scss";
 
 export default function Donut(props) {
     const { indicator, selectedCountry, selectedCountryLabel, timeFilteredData } = props;
     const val = getBlockVisValue(timeFilteredData, indicator, selectedCountry);
 
     return (
-        <div>
+        <div className={styles.donut}>
             <h3>{indicator.label}</h3>
-            <h2>{val}%</h2>
-            <h4>{selectedCountryLabel} average %</h4>
+            <h2>{indicator.format(val)}</h2>
+            <h4>{selectedCountryLabel} average</h4>
         </div>
     );
 }
