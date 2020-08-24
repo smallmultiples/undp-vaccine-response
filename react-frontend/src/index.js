a1c9cb9b57d540998eb09462992ca2baf1ff8216
Harry Morris <harry.t.morris@gmail.com>
2020-08-24 11:12:54 +1000

    use URL params instead

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -5,34 +5,61 @@ import * as serviceWorker from "./serviceWorker";
 import ReactGA from "react-ga";
 import Country from "./pages/country/country";
 import BucketEmbed from "./pages/bucket-embed/bucket-embed";
+import qs from "qs";
 
 const trackingId = "UA-25119617-15";
 ReactGA.initialize(trackingId);
 
 function getEl(embedSelector) {
     if (typeof embedSelector === "string") {
-        return document.querySelector(embedSelector);
+        const el = document.querySelector(embedSelector);
+        return el;
     } else {
         return embedSelector;
     }
 }
 
-window._UNDPDataPlatformEmbedGoal = function (embedSelector, pillarSlug, bucketIndex) {
-    ReactDOM.render(
-        <BucketEmbed pillarSlug={pillarSlug} bucketIndex={bucketIndex} />,
-        getEl(embedSelector)
-    );
-};
+function getUrlParams() {
+    const { pathname, search } = window.location;
 
-window._UNDPDataPlatformEmbedCountry = function (embedSelector, countryCode) {
-    ReactDOM.render(<Country countryCode={countryCode} />, getEl(embedSelector));
-};
+    // Pillar ->
+    if (pathname.startsWith("/undps-response")) {
+        const split = pathname.replace("/undps-response/", "").split("/");
+        const pillar = split[0];
+        const bucketSlug = split[1];
+        const query = qs.parse(search.replace("?", ""));
 
-window._UNDPDataPlatformEmbedTest = function (embedSelector) {
-    ReactDOM.render(<Root />, getEl(embedSelector));
-};
+        if (!pillar) return null;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        // TODO: convert pillar
+        const pillarSlug = pillar;
+
+        // TODO: slug to index.
+        const bucketIndex = bucketSlug ? 0 : 0;
+
+        // TODO: slug to country code.
+        const countryCode = query.country;
+
+        return {
+            type: "bucket",
+            pillarSlug: pillar,
+            bucketIndex: bucketSlug,
+            countryCode,
+        };
+    }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    const params = getUrlParams();
+
+    if (params) {
+        if (params.type === "bucket") {
+            ReactDOM.render(<BucketEmbed {...params} />, getEl("[data-bucket-embed]"));
+        }
+
+        if (params.type === "country") {
+            // TODO: ignoring for now.
+            ReactDOM.render(<Country countryCode={"AU"} />, getEl("[data-country-embed]"));
+        }
+    }
+});
