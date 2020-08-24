65ce9920990c46762e9cea9d5e3c8053d27008e1
Harry Morris <harry.t.morris@gmail.com>
2020-08-24 13:07:00 +1000

    tidy up + use exceptions + country embed

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -18,31 +18,34 @@ function getEl(embedSelector) {
     }
 }
 
+function slugToCountryCode(slug, allowEmpty = true) {
+    if (!slug) {
+        if (allowEmpty) {
+            return null;
+        } else {
+            throw new Error(`Empty country slug provided.`);
+        }
+    }
+    const countryRow = regionLookup.find(d => d["CMS slug"] === slug);
+    if (!countryRow) {
+        throw new Error(`Could not find country code for slug "${slug}".`);
+    }
+    return countryRow["ISO-alpha3 Code"];
+}
+
 function getUrlParams() {
     const { pathname, search } = window.location;
 
-    // Pillar ->
+    // Pillars
     if (pathname.startsWith("/undps-response")) {
         const split = pathname.replace("/undps-response/", "").split("/");
         const pillarSlug = split[0];
         const bucketSlug = split[1] || null;
         const query = qs.parse(search.replace("?", ""));
 
-        if (!pillarSlug) {
-            console.error(`Could not parse pillar "${pillarSlug}".`);
-            return null;
-        }
+        if (!pillarSlug) throw new Error(`Could not parse pillar "${pillarSlug}".`);
 
-        // TODO: slug to country code.
-        let countryCode = null;
-        if (query.country) {
-            const countryRow = regionLookup.find(d => d["CMS slug"] === query.country);
-            if (!countryRow) {
-                console.error(`Could not find country code for slug "${query.country}".`);
-                return null;
-            }
-            countryCode = countryRow["ISO-alpha3 Code"];
-        }
+        const countryCode = slugToCountryCode(query.country);
 
         return {
             type: "bucket",
@@ -51,22 +54,34 @@ function getUrlParams() {
             countryCode,
         };
     }
+
+    if (pathname.startsWith("/country")) {
+        const split = pathname.replace("/country/", "").split("/");
+
+        const countrySlug = split[0];
+        const countryCode = slugToCountryCode(countrySlug, false);
+
+        return {
+            type: "country",
+            countryCode,
+        };
+    }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
     const params = getUrlParams();
 
-    console.log("Embed Params");
-    console.table(params);
-
     if (params) {
+        console.log("Embed Params");
+        console.table(params);
+
         if (params.type === "bucket") {
             ReactDOM.render(<BucketEmbed {...params} />, getEl("[data-bucket-embed]"));
         }
 
         if (params.type === "country") {
             // TODO: ignoring for now.
-            ReactDOM.render(<Country countryCode={"AU"} />, getEl("[data-country-embed]"));
+            ReactDOM.render(<Country {...params} />, getEl("[data-country-embed]"));
         }
     }
 });
