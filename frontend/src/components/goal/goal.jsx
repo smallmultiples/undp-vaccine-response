768ccbdf52e9165cb3c3ddb6a5410ab8562c24f4
Andrea Lau <andrea@small.mu>
2021-06-22 14:37:10 +1000

    don't show timeline if there's only one timespan' don't show year dropdown if there's only one year.

--- frontend/src/components/goal/goal.jsx
+++ frontend/src/components/goal/goal.jsx
@@ -262,9 +262,11 @@ export default function Goal(props) {
                     />
                 </div>
             </div>
-            <div className={styles.timeArea}>
-                <Timeline timelineState={timelineState} />
-            </div>
+            {timelineState.timespan > 1 && (
+                <div className={styles.timeArea}>
+                    <Timeline timelineState={timelineState} />
+                </div>
+            )}
             <ChartArea
                 goalDatasets={goalDatasets}
                 regionLookup={regionLookup}
@@ -373,6 +375,7 @@ const ChartArea = props => {
                     className={styles.indicatorSelector}
                     noGap
                 />
+                { yearsArray.length > 1 && (
                 <Select
                     options={yearsArray}
                     onChange={x => setYear(x)}
@@ -384,6 +387,7 @@ const ChartArea = props => {
                     className={styles.yearSelector}
                     noGap
                 />
+                )}
                 <Legend />
             </div>
             {chart}
